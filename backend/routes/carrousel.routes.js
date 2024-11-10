e9b2df8a167e3a0a4e9d3c4f68e039406aaf1238
QuynhHuongVuPXL <quynhhuong.vu@student.pxl.be>
2024-11-10 12:55:52 +0100

    s3

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const carrouselRouter = express.Router();
-const carrouselData = require('./../data/carrousel.json');
+const AWS = require('aws-sdk');
 
-carrouselRouter.get('', (req, res) => {
-    res.json(carrouselData);
+const s3 = new AWS.S3();
+const BUCKET_NAME = process.env.S3_BUCKET;
+const REGION = process.env.AWS_REGION || 'us-west-2';
+
+async function getImageUrlsFromS3Bucket() {
+    const params = {
+        Bucket: BUCKET_NAME,  
+    };
+
+    try {
+        console.log('Fetching images from S3 bucket...');
+        const data = await s3.listObjectsV2(params).promise();
+        console.log('Raw data from S3:', data); // Log the full data to inspect Contents
+        const images = data.Contents
+            .filter(item => item.Size > 0 && item.Key.match(/\.(jpeg|jpg|gif|png)$/i)) 
+            .map(item => {
+                const imageUrl = `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/${item.Key}`; 
+                return { url: imageUrl };
+            });
+        console.log('Filtered images:', images); // Log the final image array to check URLs
+        return images;
+    } catch (error) {
+        console.error('Error fetching images from S3 bucket:', error);
+        return [];
+    }
+}
+
+carrouselRouter.get('', async (req, res) => {
+    try {
+        const imageUrls = await getImageUrlsFromS3Bucket();
+        res.json(imageUrls);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
-
 
 module.exports = carrouselRouter;
