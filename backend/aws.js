2788fcf19acb5a9f02bae16bac3664599074ec21
QuynhHuongVuPXL <116728967+QuynhHuongVuPXL@users.noreply.github.com>
2024-11-10 13:58:44 +0100

    bug fix

--- backend/aws.js
+++ backend/aws.js
@@ -1,16 +1,5 @@
 const AWS = require('aws-sdk');
-
-/*
-# These settings can be used if no propper IAM roles are set up or when running locally
-AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID});
-AWS.config.update({secretAccessKey: process.env.AWS_ACCESS_KEY});
-AWS.config.update({region: process.env.AWS_REGION});
-*/
-
-AWS.config.update({
-  region: process.env.AWS_REGION || 'us-west-2'
-});
-
+AWS.config.update({ region: process.env.AWS_REGION || 'us-west-2' });
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports = docClient;
